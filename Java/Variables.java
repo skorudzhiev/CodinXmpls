fdcb80c8efb1d8fdcc84e51c194a09a90cefb60e
Stoyan Korudzhiev <skorudzhiev@gmail.com>
2018-07-15 16:13:06 +0300

    Create Variables.java

---
+++ Java/Variables.java
@@ -0,0 +1,17 @@
+// Declaring variables
+boolean result;
+char capitalC;
+byte b;
+short s;
+int i;
+String text;
+
+// Storing data in a Variables
+result = true;
+capitalC = 'C';
+b = 100;
+s = 10000;
+i = 100000;
+text = "Random text";
+
+// Retrieving data from Variables
